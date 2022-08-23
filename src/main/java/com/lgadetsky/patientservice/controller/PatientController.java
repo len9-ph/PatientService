9f8fa42c8612c78a9d160ab457daa1c9fea00056
Leonid Gadetsky <leonidgadetsky@yandex.ru>
2022-08-23 14:04:14 +0300

    add controller

--- src/main/java/com/lgadetsky/patientservice/controller/PatientController.java
+++ src/main/java/com/lgadetsky/patientservice/controller/PatientController.java
@@ -1,8 +1,48 @@
 package com.lgadetsky.patientservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lgadetsky.patientservice.model.Patient;
+import com.lgadetsky.patientservice.service.PatientService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 public class PatientController {
+	@Autowired
+	private PatientService patientService;
+	
+	@PostMapping("/patient")
+	ResponseEntity<?> create(@RequestBody Patient patient){
+		patientService.create(patient);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@GetMapping("/patient/{id}")
+	ResponseEntity<?> readById(@PathVariable int id) {
+		return new ResponseEntity<Patient>(patientService.findById(id), HttpStatus.OK);	
+	}
+	
+	@PutMapping("/patient/{id}")
+	ResponseEntity<?> update(@PathVariable int id, @RequestBody Patient patient) {
+		patient.setId(id);
+		patientService.update(patient);
+		return new ResponseEntity<Patient>(patient, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/patient/{id}")
+	ResponseEntity<?> deleteByid(@PathVariable int id){
+		patientService.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
 	
 }
