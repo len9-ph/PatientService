a8723dce30595d0a5442ff5bb688a4bd859c3b8f
leonid <leonidgadetsky@yandex.ru>
2022-08-31 01:06:38 +0300

    some javadocs

--- src/main/java/com/lgadetsky/patientservice/service/Service.java
+++ src/main/java/com/lgadetsky/patientservice/service/Service.java
@@ -1,5 +1,12 @@
 package com.lgadetsky.patientservice.service;
 
+/**
+ * Generic service
+ * 
+ * @author Leonid Gadetsky
+ * @param <T> - object type 
+ * @param <E> - object id type
+ */
 public interface Service<T, E> {
 	E create(T obj);
 	T findById(E id);
